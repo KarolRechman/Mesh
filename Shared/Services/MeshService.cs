94a0be11c5acfd6bac698075d41a8dab4d7502aa
Karol Rechman <39775135+KarolRechman@users.noreply.github.com>
2023-05-13 12:43:39 +0200

    Add files via upload

---
+++ Shared/Services/MeshService.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Services;
+
+class MeshService : IMeshService
+{
+	var formdata = await req.ReadFormAsync();
+	var file = formdata.Files["mesh"];
+	int numberOfSpots = int.Parse(req.Form["numberOfSpots"]);
+}
